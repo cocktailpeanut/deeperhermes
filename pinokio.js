0086323857a1237fbff20f2728284c7b3bef7921
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-14 14:09:20 -0500

    update

--- pinokio.js
+++ pinokio.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports = {
   version: "3.6",
   title: "Deeper Hermes",
-  description: "deephermes that doesn't need system prompt, autonomously responds based on its own judgment",
+  description: "deephermes that doesn't need system prompt, autonomously responds based on its own judgment https://huggingface.co/NousResearch/DeepHermes-3-Llama-3-8B-Preview",
   icon: "icon.jpeg",
   pre: [{
     icon: "lmstudio.jpeg",
